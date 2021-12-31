1bbca55bde530ae9cc2f31582fae83417bbc360d
Yuliia Besedina <92574244+bjula@users.noreply.github.com>
2021-12-31 03:01:21 +0200

    add script mobile-menu, mob menu

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
